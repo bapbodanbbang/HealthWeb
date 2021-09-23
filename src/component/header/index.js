a60e61f60e8dc9e0c30003188d048e3ba941e978
homeswiz <homes@wizschool.io>
2021-09-23 23:16:22 +0900

    Add header structure

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,8 +1,34 @@
+import "./index.scss";
+
 function Header () {
   return (
-    <div>
-        header
-    </div>
+    <header className="header">
+      <div className="contents">
+        <div>
+          로고 자리
+        </div>
+
+        <nav className="navigation">
+          <ul>
+            <li>
+              메뉴 1
+            </li>
+            <li>
+              메뉴 2
+            </li>
+            <li>
+              메뉴 2
+            </li>
+            <li>
+              회원 아이콘
+            </li>
+            <li>
+              알림 로고
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
