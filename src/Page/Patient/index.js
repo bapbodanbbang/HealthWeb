2a0268f1914338a6a0688f00d002327ac8e916c0
homeswiz <homes@wizschool.io>
2021-09-25 00:43:20 +0900

    Add scss in Patient Page

--- src/Page/Patient/index.js
+++ src/Page/Patient/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../../Component/Header';
-import Footer from '../../Component/Footer';
+import "./index.scss";
 
 function Patient () {
   return (
     <div className="Patient">
       <Header/>
-      <Footer/>
+      <div>
+        <div className="Section_A">
+          <div>name</div>
+          <div className="List">
+            <div>test</div>
+            <div>test2</div>
+          </div>
+        </div>
+        <div className="Section_B">
+          <div>name</div>
+          <div>test3</div>
+        </div>
+      </div>
     </div>
   );
 }
