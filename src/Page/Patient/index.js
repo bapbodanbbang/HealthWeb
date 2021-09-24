a85207c9bc056f6b6598c4f63244f15e7c1c9d42
homeswiz <homes@wizschool.io>
2021-09-25 00:46:03 +0900

    Add Component Prescription

--- src/Page/Patient/index.js
+++ src/Page/Patient/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Header from '../../Component/Header';
+import Prescription from '../../Component/Prescription';
 import "./index.scss";
 
 function Patient () {
   return (
     <div className="Patient">
       <Header/>
-      <div>
-        <div className="Section_A">
-          <div>name</div>
-          <div className="List">
-            <div>test</div>
-            <div>test2</div>
-          </div>
-        </div>
-        <div className="Section_B">
-          <div>name</div>
-          <div>test3</div>
-        </div>
-      </div>
+      <Prescription/>
     </div>
   );
 }
---
+++ src/component/Prescription/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./index.scss";
+
+function Prescription () {
+  return (
+      <div>
+        <div className="Section_A">
+          <div>name</div>
+          <div className="List">
+            <div>test</div>
+            <div>test2</div>
+          </div>
+        </div>
+        <div className="Section_B">
+          <div>name</div>
+          <div>test3</div>
+        </div>
+      </div>
+  );
+}
+
+export default Prescription;
