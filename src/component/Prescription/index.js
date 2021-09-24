0f0091fba821318843f1efb232e05c55029345fb
homeswiz <homes@wizschool.io>
2021-09-25 00:53:08 +0900

    modify space 4 -> 2

--- src/component/Prescription/index.js
+++ src/component/Prescription/index.js
@@ -5,15 +5,15 @@ function Prescription () {
   return (
   <div className="Prescription">
     <div className="Section_A">
-    <div>name</div>
-    <div className="List">
-      <div>test</div>
-      <div>test2</div>
-    </div>
+      <div>name</div>
+      <div className="List">
+        <div>test</div>
+        <div>test2</div>
+      </div>
     </div>
     <div className="Section_B">
-    <div>name</div>
-    <div>test3</div>
+      <div>name</div>
+      <div>test3</div>
     </div>
   </div>
   );
