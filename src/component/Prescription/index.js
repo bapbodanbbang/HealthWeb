bb1466ca55b3cb251396ef57f2d3220b19d5c4a5
homeswiz <homes@wizschool.io>
2021-09-25 00:47:19 +0900

    Delete space 4

--- src/component/Prescription/index.js
+++ src/component/Prescription/index.js
@@ -3,19 +3,19 @@ import "./index.scss";
 
 function Prescription () {
   return (
-      <div>
-        <div className="Section_A">
-          <div>name</div>
-          <div className="List">
-            <div>test</div>
-            <div>test2</div>
-          </div>
-        </div>
-        <div className="Section_B">
-          <div>name</div>
-          <div>test3</div>
+    <div>
+      <div className="Section_A">
+        <div>name</div>
+        <div className="List">
+          <div>test</div>
+          <div>test2</div>
         </div>
       </div>
+      <div className="Section_B">
+        <div>name</div>
+        <div>test3</div>
+      </div>
+    </div>
   );
 }
 
