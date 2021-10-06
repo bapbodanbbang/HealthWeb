f1a424757b0902be6132701eaf35cce53e1f4eb0
homeswiz <homes@wizschool.io>
2021-10-06 23:03:50 +0900

    Complete

--- src/component/Prescription/index.js
+++ src/component/Prescription/index.js
@@ -5,7 +5,15 @@ import loadingIcon from "../../images/loading.png";
 
 function Prescription () {
 
-  const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([{
+    name: "임지영",
+    phone: "010-6320-8161",
+    medicines: [
+      "가브스정 50 밀리그램(빌다클립틴)",
+      "가리유리점안액(피레녹산)"
+    ],
+    time: "2021/10/07 11:00:00"
+  }]);
   const [patients, setPatients] = useState([{
     name: "유진선",
     phone: "010-4479-8650",
@@ -13,28 +21,43 @@ function Prescription () {
       "가스모틴정",
       "가브스정 50 밀리그램(빌다클립틴)",
       "가리유리점안액(피레녹산)"
-    ]
+    ],
+    time: "2021/10/07 11:00:00"
   },{
     name: "김민재",
     phone: "010-5193-9519",
-    medicines: [ "타이레놀" ]
+    medicines: [ "타이레놀" ],
+    time: "2021/10/07 13:40:00"
   }]);
 
-  const [medicineList, setMedicineList] = useState([]);
+  const [medicineList, setMedicineList] = useState([
+    "가브스정 50 밀리그램(빌다클립틴)",
+    "가리유리점안액(피레녹산)"
+  ]);
   const [userHighlight, setUserHighlight] = useState(0);
 
   const onClickSelect = () => {
     setUserList([...userList, patients[0]])
-    patients.splice(0,1)
-    setPatients(patients);
+    const result = patients;
+    result.splice(0,1);
+    console.log("result: ",result);
+    setPatients(result);
   }
 
   const onClickUnSelect = () => {
-    patients.splice(0,1)
-    setPatients(patients)
+    setUserList([...userList])
+    const result = patients;
+    result.splice(0,1);
+    console.log("result: ",result);
+    setPatients(result);
   }
 
   useEffect(() => {
+    console.log("patients: ",patients);
+  },[patients])
+
+  useEffect(() => {
+    console.log("userList: ",userList);
   },[userList])
 
   useEffect(() => {
@@ -50,9 +73,11 @@ function Prescription () {
         {patients.length
           ? (
           <><div className="Prescription_Header">
-              <img src={userIcon} />
-              <div>
-                <div>{patients[0].name}</div>
+              <div className="User_Icon">
+                <img src={userIcon}/>
+              </div>
+              <div className="User">
+                <div className="User_Name">{patients[0].name}</div>
                 <div>{patients[0].phone}</div>
               </div>
             </div>
@@ -60,13 +85,13 @@ function Prescription () {
               {patients[0].medicines.map(value => <div className="Medicine">{value}</div>)}
             </div>
             <div className="Button_Div">
-              <button className="Button" title="조제 가능" onClick={onClickSelect} />
-              <button className="Button" title="조제 불가능" onClick={onClickUnSelect} />
+              <div className="Button" onClick={onClickSelect}>조제 가능</div>
+              <div className="Button" onClick={onClickUnSelect}>조제 불가능</div>
             </div></>) 
           : (
             <div className="loading_div">
-              <img className="loading" src={loadingIcon}/>
               <div className="loading_text">환자를 기다리는 중...</div>
+              <img className="loading" src={loadingIcon} color="#000000"/>
             </div>
             )}
       </div>
@@ -75,8 +100,11 @@ function Prescription () {
           {userList.map((user,index) => 
             <div 
               key={index} 
-              className="Name"
-              onClick={() => setMedicineList(userList[index].medicines)}
+              className= {userHighlight == index ? "Name_Selected" : "Name"}
+              onClick={() => {
+                setUserHighlight(index);
+                setMedicineList(userList[index].medicines)
+              }}
             >
               {user.name}
             </div>)
@@ -87,7 +115,13 @@ function Prescription () {
           {medicineList.map((medicine, index) => 
             <div key={index} className="MakeMedicine">{medicine}</div>
           )}
-          <div className="Medicine_Complete">조제 완료</div>
+          <div className="Medicine_Complete" onClick={() => {
+            setMedicineList(userList[userHighlight-1].medicines)
+            setUserHighlight(userHighlight-1);
+            const result = userList;
+            result.splice(userHighlight, 1);
+            setUserList(result);
+          }}>조제 완료</div>
         </div>
       </div>
     </div>
--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,32 +1,36 @@
 import "./index.scss";
-import Polygon from "../../images/Polygon 3.png";
+import arrowIcon from "../../images/arrow.png";
+import logo from "../../images/logo.png";
 
 function Header () {
   return (
     <header className="header">
       <div className="contents">
-        <div>
-          로고 자리
-        </div>
+        <img src={logo} className="logo"/>
         <nav className="navigation">
           <ul>
+          <div className="menu">
+              <li className="home">
+                홈
+              </li>
+            </div>
             <div className="menu">
               <li>
                 환자 관리
               </li>
-              <img src={Polygon} width="20px"/>
+              <img src={arrowIcon} width="15px"/>
             </div>
             <div className="menu">
               <li>
                 의약품 교품거래
               </li>
-              <img src={Polygon} width="20px"/>
+              <img src={arrowIcon} width="15px"/>
             </div>
             <div className="menu">
               <li>
                 의약품 관리
               </li>
-              <img src={Polygon} width="20px"/>
+              <img src={arrowIcon} width="15px"/>
             </div>
           </ul>
         </nav>
